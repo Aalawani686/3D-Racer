50fadf23735757320a8a927e773e8c55f345c4cd
akhanwale <45947473+akhanwale@users.noreply.github.com>
2018-12-18 09:16:59 -0800

    Add files via upload

---
+++ Tester.java
@@ -0,0 +1,13 @@
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Driver d = new Driver();
+		System.out.println(d.Counter("If someone were to ask me, \\\"Which is better: improving quality of life for about 32% of the world(who already lead cushy lives), or improving it vastly for 68% of the population(who live in impoverishment)\\\", I would obviously be for the second. This is the kind of situation presented by the Grand Challenges. \n" + 
+				"\"Pageant answer though it may be, the world needs to focus on improving access to water. Reviewing each of the 14 challenges, the only commonality I see in them is that they will be difficult and require mass coordination to achieve: which makes sense, seeing as they are \"Grand\". Yet there is only one which, in my opinion, carries real gravity and immediacy- clean water.\n" + 
+				"\n" + 
+				" "));
+	}
+
+}
